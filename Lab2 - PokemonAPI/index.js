2869144e7647e311eb83bfd84264b7c2c228ee65
carooobregon <carooobregon@gmail.com>
2021-03-14 22:56:07 -0700

    finished pokeapi

--- Lab2 - PokemonAPI/index.js
+++ Lab2 - PokemonAPI/index.js
@@ -1,12 +1,12 @@
 var myForm = document.getElementById("myForm");
 const pokemonName = document.getElementById("pokemonName");
-var elements  = document.getElementById("elements");
+var elements  = document.getElementById("pokeRow");
+var alertBoot = document.getElementById("myAlert");
 const prefixUrl = "https://pokeapi.co/api/v2/pokemon/";
 
 myForm.onsubmit = (event) => {
     event.preventDefault();
-    let url = prefixUrl + pokemonName.value;
-    console.log(url);
+    let url = prefixUrl + pokemonName.value.toLowerCase();
     let ajaxPromise = new Promise((resolve, reject) => {
         // Initialize the HTTP request.
         var xhr = new XMLHttpRequest();
@@ -20,6 +20,7 @@ myForm.onsubmit = (event) => {
             if (xhr.status === OK) {
               resolve(xhr.responseText);
             } else {
+                alertBoot.textContent = "Pokemon not found!";
               reject(xhr.status);
             }
           }
@@ -30,10 +31,47 @@ myForm.onsubmit = (event) => {
       
       let p = document.querySelector("p");
       ajaxPromise.then((okMessage) => {
-        var obj = JSON.parse(okMessage);
-        console.log(obj);
-        p.innerHTML = okMessage
+            var obj = JSON.parse(okMessage);
+            let colItem =  document.createElement("div");
+            colItem.setAttribute("class", "col");
+            colItem.style.margin = '20px';
+            elements.appendChild(colItem);
+
+            let newitem=document.createElement("div");
+            newitem.setAttribute("class", "card");
+            newitem.style.width = '200px';
+            colItem.appendChild(newitem);
+            let cardPic = document.createElement("img");
+            cardPic.setAttribute("class", "card-img-top");
+            cardPic.src = obj.sprites.front_default;
+            newitem.appendChild(cardPic);
+            let pokeInfo = document.createElement("div");
+            pokeInfo.setAttribute("class", "card-body");
+            newitem.appendChild(pokeInfo);
+
+            let pokeName = document.createElement("h4");
+            pokeName.setAttribute("class", "card-title");
+            pokeName.textContent = obj.name;
+            pokeInfo.appendChild(pokeName);
+
+            let pokeExp = document.createElement("p");
+            pokeExp.setAttribute("class", "card-text");
+            pokeExp.textContent = "Experience : " + obj.base_experience ;
+            pokeInfo.appendChild(pokeExp);
+
+            let pokeHeight = document.createElement("p");
+            pokeHeight.setAttribute("class", "card-text");
+            pokeHeight.textContent = "Height : " + obj.height ;
+            pokeInfo.appendChild(pokeHeight);
+
+            let pokeWeight = document.createElement("p");
+            pokeWeight.setAttribute("class", "card-text");
+            pokeWeight.textContent = "Weight : " + obj.weight ;
+            pokeInfo.appendChild(pokeWeight);
+
+
         })
       .catch(err =>  {p.innerHTML = err })
+      myForm.reset();
+      alertBoot.textContent = "";
 };
-
