b8854fc57e6d676d83248f24e0c3189900a08525
carooobregon <carooobregon@gmail.com>
2021-04-10 09:11:22 -0500

    api req working

--- Lab3-NodeJS_PokemonAPI/lab.js
+++ Lab3-NodeJS_PokemonAPI/lab.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 const port = 3000;
+const axios = require('axios').default;
 
 app.get('/', (req, res) => {
   var options = {
@@ -23,7 +24,17 @@ app.route('/addPokemon').post(function(req, res){
 });
 
 app.route('/addPokemon').get(function(req, res){
-  console.log(req.query.pokemonName);
+  const URL = 'https://pokeapi.co/api/v2/pokemon/' + req.query.pokemonName;
+
+  axios.get(URL)
+  .then(pokemon_response => {
+      let pokemon_data = pokemon_response.data;
+      console.log("I am making a request to " + URL);
+      res.send(pokemon_data);
+  }).catch(function(error){
+      console.log(error);
+      res.status(404).send('the pokemon does not exist');
+  });
 });
 
 
