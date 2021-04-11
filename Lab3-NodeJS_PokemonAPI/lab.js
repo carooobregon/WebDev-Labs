b23fb14e649e229c459c1daaeb9b315f653bd34d
carooobregon <carooobregon@gmail.com>
2021-04-11 08:29:14 -0500

    cards working

--- Lab3-NodeJS_PokemonAPI/lab.js
+++ Lab3-NodeJS_PokemonAPI/lab.js
@@ -5,6 +5,10 @@ const app = express();
 const port = 3000;
 const axios = require('axios').default;
 
+var currPokemons = [];
+app.set('views', './views')
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
   var options = {
     root: path.join(__dirname, 'public'),
@@ -15,11 +19,11 @@ app.get('/', (req, res) => {
     }
   }
   var fileName = "page.html"
-  res.sendFile(fileName, options);
+  res.render('template', {currPokemons});
 });
 
 app.route('/addPokemon').post(function(req, res){
-  // let nameParam = req.body.pokemonName;
+  let nameParam = req.body.pokemonName;
   res.end('Looking for pokemon ' + nameParam);
 });
 
@@ -30,9 +34,9 @@ app.route('/addPokemon').get(function(req, res){
   .then(pokemon_response => {
       let pokemon_data = pokemon_response.data;
       console.log("I am making a request to " + URL);
-      res.send(pokemon_data);
-      console.log(pokemon_data.name);
-  }).catch(function(error){
+      currPokemons.push({name:pokemon_data.name, pic:pokemon_data.sprites.front_default});
+      res.render('template', {currPokemons});
+    }).catch(function(error){
       console.log(error);
       res.status(404).send('the pokemon does not exist');
   });
